ca2d01d8cc83612a79a8acd403d5cd8205205a79
GwangGeun-Jung <gwanggeun.jung@deliveryhero.com>
2022-03-21 13:09:53 +0100

    test

--- src/main/kotlin/com/example/demo2/controller/PingController.kt
+++ src/main/kotlin/com/example/demo2/controller/PingController.kt
@@ -2,6 +2,8 @@ package com.example.demo2.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.client.WebClient
+import java.net.http.HttpClient
 
 @RestController
 class PingController {
